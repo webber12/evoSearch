cbace41e4a0f400a9d20ed1dc562ed0f85611377
webber12 <web-ber12@yandex.ru>
2014-07-13 15:44:16 +0200

    SF

--- assets/snippets/evoSearch/evoSearch.snippet.php
+++ assets/snippets/evoSearch/evoSearch.snippet.php
@@ -20,8 +20,8 @@ if (isset($_GET['search']) && $_GET['search'] != '') {
     $eSS->params['documents'] = $documents;
     $eSS->params['display'] = isset($eSS->params['display']) ? $eSS->params['display'] : "20";
     $eSS->params['paginate'] = isset($eSS->params['paginate']) ? $eSS->params['paginate'] : "pages";
-	$eSS->params['show_stat'] = isset($eSS->params['show_stat']) ? $eSS->params['show_stat'] : "1";
-	$eSS->params['statTpl'] = isset($eSS->params['statTpl']) ? $eSS->params['statTpl'] : '<div class="evoSearch_info">По запросу <b>[+stat_request+]</b> найдено всего <b>[+stat_total+]</b>. Показано <b>[+stat_display+]</b>, c [+stat_from+] по [+stat_to+]</div>';
+    $eSS->params['show_stat'] = isset($eSS->params['show_stat']) ? $eSS->params['show_stat'] : "1";
+    $eSS->params['statTpl'] = isset($eSS->params['statTpl']) ? $eSS->params['statTpl'] : '<div class="evoSearch_info">По запросу <b>[+stat_request+]</b> найдено всего <b>[+stat_total+]</b>. Показано <b>[+stat_display+]</b>, c [+stat_from+] по [+stat_to+]</div>';
     $worker = isset($eSS->params['worker']) ? $eSS->params['worker'] : "DocLister";
 
     $words_original = $eSS->makeWordsFromText($eSS->Get('txt_original'));
