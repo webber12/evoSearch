a2af8afb5836c82c3b62a49777cae6e6bfac5b45
webber12 <web-ber12@yandex.ru>
2015-04-18 15:40:14 +0200

    убираем дополнительное $where за пределы цикла в плагине

--- assets/plugins/evoSearch/evoSearch.plugin.php
+++ assets/plugins/evoSearch/evoSearch.plugin.php
@@ -11,15 +11,15 @@ if ($e->name == 'OnDocFormSave') {
     $sql = $eSP->makeSQLForSelectWords();
     //echo $sql;die();
     $q = $eSP->modx->db->query($sql);
+    $where = '';
+    $where .= ($eSP->params['excludeTmpls'] != '' ? ' template NOT IN(' . $eSP->params['excludeTmpls'] . ') ' : '');
+    $where .= ($eSP->params['excludeIDs'] != '' ? ($where != '' ? ' OR ' : '') . ' id NOT IN(' . $eSP->params['excludeIDs'] . ') ' : '');
+    $where = !empty($where) ? ' AND (' . $where . ')'; 
     while ($row = $eSP->modx->db->getRow($q)) {
         $content_original = $row['pagetitle'] . ' ' . $row['longtitle'] . ' ' . $row['description'] . ' ' . $row['introtext'] . ' ' . $row['content'];
         $content = $eSP->modx->stripTags($content_original);
         $content = $eSP->injectTVs($row['id'], $content);
         $words = $eSP->Words2BaseForm(mb_strtoupper($content, 'UTF-8'));
-        $where = '';
-        $where .= ($eSP->params['excludeTmpls'] != '' ? ' template NOT IN(' . $eSP->params['excludeTmpls'] . ') ' : '');
-        $where .= ($eSP->params['excludeIDs'] != '' ? ($where != '' ? ' OR ' : '') . ' id NOT IN(' . $eSP->params['excludeIDs'] . ') ' : '');
-        $where = !empty($where) ? ' AND (' . $where . ')'; 
         $upd = $eSP->modx->db->update(
             array($eSP->ext_content_field => $eSP->modx->db->escape($content), $eSP->ext_content_index_field => $eSP->modx->db->escape($words)),
             $eSP->content_table,
